13085af91066d78e5d651763f9ad71d454652b73
Matheus Henrique de Oliveria <62122558+Mthws167@users.noreply.github.com>
2022-04-07 07:48:13 -0300

    Update dart_application.dart

--- Projeto/dart_application/bin/dart_application.dart
+++ Projeto/dart_application/bin/dart_application.dart
@@ -1,11 +1,14 @@
 //import 'package:dart_application/primeiro.dart' as primeiro;
 //import 'package:dart_application/Teste.dart' as teste;
 //import 'package:dart_application/Teste2.dart' as teste2;
-import 'package:dart_application/Teste3.dart' as teste3;
+//import 'package:dart_application/Teste3.dart' as teste3;
+import 'package:dart_application/Teste4.dart' as teste4;
 
 void main(List<String> arguments) {
+
   //primeiro.funcao();
-  teste3.estruturaDecisao();
+  teste4.conceitoAluno();
+  //teste3.estruturaDecisao();
   //teste2.dadosDiscilplina();
   //teste.solicitarDados();
 }
