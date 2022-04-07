6da50ac2ade8a90484873783fa493481a29601b2
Matheus Henrique de Oliveria <62122558+Mthws167@users.noreply.github.com>
2022-04-07 10:29:50 -0300

    Update Teste4.dart

--- Projeto/dart_application/lib/Teste4.dart
+++ Projeto/dart_application/lib/Teste4.dart
@@ -1,10 +1,13 @@
 import 'dart:io';
 
 void conceitoAluno() {
+  String aprovado = '';
+  String reprovado = '';
+
   print("Informe o nome do Aluno:");
   var aluno = stdin.readLineSync();
 
-  print("Informe as 4 notas do aluno:");
+  print("Informe as 4 notas do aluno(de 0 a 10):");
   var nota1 = stdin.readLineSync()!;
   var nota2 = stdin.readLineSync()!;
   var nota3 = stdin.readLineSync()!;
@@ -15,39 +18,133 @@ void conceitoAluno() {
   int nota03 = int.parse(nota3);
   int nota04 = int.parse(nota4);
 
-  if (nota01 < 0 && nota01 > 10) {
-    print("Informe números de 0 a 10!");
-  } else if (nota02 < 0 && nota02 > 10) {
-    print("Informe números de 0 a 10!");
-  } else if (nota03 < 0 && nota03 > 10) {
-    print("Informe números de 0 a 10!");
-  } else if (nota04 < 0 && nota04 > 10) {
-    print("Informe números de 0 a 10!");
-  }
-
   double media = (nota01 + nota02 + nota03 + nota04) / 4;
-  String aprovado = '';
-  String reprovado = '';
-  if (media >= 10 * 0.6) {
-    aprovado = "Aprovado!";
-  } else {
-    reprovado = "Reprovado!";
-  }
 
-  print('''
-  ${aluno}:
+  if (nota01 < 6) {
+    print("Informe a nota da recuperação 1: ");
+    var rec1 = stdin.readLineSync()!;
+    int rec01 = int.parse(rec1);
+    double media = (rec01 + nota02 + nota03 + nota04) / 4;
+    if (media >= 10 * 0.6) {
+      aprovado = "Aprovado!";
+    } else {
+      reprovado = "Reprovado!";
+    }
+    print('''
+          ${aluno}:
 
-  Notas:
-    ${nota01}
-    ${nota02}
-    ${nota03}
-    ${nota04}
+          Notas:
+             ${rec01}
+             ${nota02}
+             ${nota03}
+             ${nota04}
 
-  Média:
-    ${media}
+          Média:
+              ${media}
 
-  --------------
-  O aluno está ${aprovado}${reprovado}
+          -------------------------
+          ${aluno} está ${aprovado}${reprovado}
     
   ''');
+  } else if (nota02 < 6) {
+    print("Informe a nota da recuperação 2: ");
+    var rec2 = stdin.readLineSync()!;
+    int rec02 = int.parse(rec2);
+    double media = (nota01 + rec02 + nota03 + nota04) / 4;
+    if (media >= 10 * 0.6) {
+      aprovado = "Aprovado!";
+    } else {
+      reprovado = "Reprovado!";
+    }
+    print('''
+          ${aluno}:
+
+          Notas:
+             ${nota01}
+             ${rec02}
+             ${nota03}
+             ${nota04}
+
+          Média:
+              ${media}
+
+          -------------------------
+          ${aluno} está ${aprovado}${reprovado}
+    
+  ''');
+  } else if (nota03 < 6) {
+    print("Informe a nota da recuperação 3: ");
+    var rec3 = stdin.readLineSync()!;
+    int rec03 = int.parse(rec3);
+    double media = (nota01 + nota02 + rec03 + nota04) / 4;
+    if (media >= 10 * 0.6) {
+      aprovado = "Aprovado!";
+    } else {
+      reprovado = "Reprovado!";
+    }
+    print('''
+          ${aluno}:
+
+          Notas:
+             ${nota01}
+             ${nota02}
+             ${rec03}
+             ${nota04}
+
+          Média:
+              ${media}
+
+          -------------------------
+          ${aluno} está ${aprovado}${reprovado}
+    
+  ''');
+  } else if (nota04 < 6) {
+    print("Informe a nota da recuperação 4: ");
+    var rec4 = stdin.readLineSync()!;
+    int rec04 = int.parse(rec4);
+    double media = (nota01 + nota02 + nota03 + rec04) / 4;
+    if (media >= 10 * 0.6) {
+      aprovado = "Aprovado!";
+    } else {
+      reprovado = "Reprovado!";
+    }
+    print('''
+          ${aluno}:
+
+          Notas:
+             ${nota01}
+             ${nota02}
+             ${nota03}
+             ${rec04}
+
+          Média:
+              ${media}
+
+          -------------------------
+          ${aluno} está ${aprovado}${reprovado}
+    
+  ''');
+  } else {
+    if (media >= 10 * 0.6) {
+      aprovado = "Aprovado!";
+    } else {
+      reprovado = "Reprovado!";
+    }
+    print('''
+          ${aluno}:
+
+          Notas:
+             ${nota01}
+             ${nota02}
+             ${nota03}
+             ${nota04}
+
+          Média:
+              ${media}
+
+          -------------------------
+          ${aluno} está ${aprovado}${reprovado}
+    
+  ''');
+  }
 }
