0e42360e9e6c47380250bdf062b40474a6c72ded
Matheus Henrique de Oliveria <62122558+Mthws167@users.noreply.github.com>
2022-04-08 01:49:25 -0300

    Add files via upload

---
+++ Projeto/dart_application/bin/dart_application.dart
@@ -0,0 +1,23 @@
+//import 'package:dart_application/dadosAluno.dart' as dadosAluno;
+//import 'package:dart_application/dadosCurso.dart' as dadosCurso;
+//import 'package:dart_application/dadosDisciplina.dart' as dadosDisciplina;
+//import 'package:dart_application/reprovadoFalta.dart' as repovadoFalta;
+//import 'package:dart_application/maiorqueaMedia.dart' as maiorMedia;
+//import 'package:dart_application/maiormenorMedia.dart' as maiormenorMedia;
+//import 'package:dart_application/conceitoFinal.dart' as conceitoFinal;
+//import 'package:dart_application/notaAluno.dart' as notaAluno;
+//import 'package:dart_application/dadosAluno3.dart' as dadosAluno3;
+//import 'package:dart_application/usuariodesejaDados.dart' as desejaDados;
+
+void main(List<String> arguments) {
+  //dadosCurso.variaveisUm();
+  //dadosAluno.variaveisDois();
+  //dadosDisciplina.variaveisTres();
+  //repovadoFalta.decisaoUm();
+  //maiorMedia.decisaoDois();
+  //maiormenorMedia.decisaoTres();
+  //conceitoFinal.decisaoQuatro();
+  //notaAluno.decisao5();
+  //dadosAluno3.repeticaoUm();
+  //desejaDados.repeticaoDois();
+}
---
+++ Projeto/dart_application/lib/dadosAluno.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+void variaveisDois() {
+  print("Informe o seu RA:");
+  var ra = stdin.readLineSync();
+
+  print("Informe o seu nome:");
+  var nome = stdin.readLineSync();
+
+  print("Informe seu ano de nascimento:");
+  var anoNascimento = stdin.readLineSync()!;
+
+  print("Informe a sua série:");
+  var serie = stdin.readLineSync();
+
+  num anoNascimento0 = num.parse(anoNascimento);
+  var idade = 2022 - anoNascimento0;
+
+  print('''
+Aluno:
+    RA: ${ra} ;
+    Nome: ${nome} ; 
+    Ano de Nascimento ${anoNascimento} ;
+    Idade: ${idade} anos ; 
+    Série: ${serie} série.
+''');
+}
