564fad5cfb658c31f429b7941fc36c3f8344a4b5
Matheus Henrique de Oliveria <62122558+Mthws167@users.noreply.github.com>
2022-04-12 22:35:08 -0300

    Update funcoes_diversas.dart

--- funcoes_diversas/bin/funcoes_diversas.dart
+++ funcoes_diversas/bin/funcoes_diversas.dart
@@ -1,15 +1,17 @@
-import 'package:funcoes_diversas/functionNoReturnNoParameter.dart'
-    as functionNoReturnNoParameter;
-import 'package:funcoes_diversas/functionNoReturnWithParameter.dart'
-    as functionNoReturnWithParameter;
-import 'package:funcoes_diversas/functionWithReturnNoParameter.dart'
-    as functionWithReturnNoParameter;
-import 'package:funcoes_diversas/functionWithReturnWithParameter.dart'
-    as functionWithReturnWithParameter;
+//import 'package:funcoes_diversas/functionNoReturnNoParameter.dart'
+//as functionNoReturnNoParameter;
+//import 'package:funcoes_diversas/functionNoReturnWithParameter.dart'
+// as functionNoReturnWithParameter;
+//import 'package:funcoes_diversas/functionWithReturnNoParameter.dart'
+//as functionWithReturnNoParameter;
+//import 'package:funcoes_diversas/functionWithReturnWithParameter.dart'
+//as functionWithReturnWithParameter;
+import 'package:funcoes_diversas/desafioFuncoes.dart' as desafio;
 
 void main(List<String> arguments) {
-  functionNoReturnNoParameter.gestorSalario();
-  functionNoReturnWithParameter.show();
-  functionWithReturnNoParameter.show();
-  functionWithReturnWithParameter.show();
+  // functionNoReturnNoParameter.gestorSalario();
+  //functionNoReturnWithParameter.show();
+  //functionWithReturnNoParameter.show();
+  //functionWithReturnWithParameter.show();
+  desafio.desafio();
 }
