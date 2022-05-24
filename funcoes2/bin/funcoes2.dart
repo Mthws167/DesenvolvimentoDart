87e9fde117bbf27a7c9c060ca6bd02a978237b0b
Matheus Henrique de Oliveria <mthws.henrique@hotmail.com>
2022-05-24 13:01:45 -0300

    Delete funcoes2 directory

--- funcoes2/bin/funcoes2.dart
+++
@@ -1,9 +0,0 @@
-//import 'package:funcoes2/funcoes2.dart' as desafio;
-//import 'package:funcoes2/atividade1.dart' as atividade1;
-import 'package:funcoes2/atividade2.dart' as atividade2;
-
-void main(List<String> arguments) {
-  //desafio.mostrar();
-  //atividade1.mostrar();
-  atividade2.mostrar();
-}
