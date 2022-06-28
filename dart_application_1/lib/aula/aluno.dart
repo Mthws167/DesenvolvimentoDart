3288a9437e9928fa970fa4088f583c0d965cf2cc
Matheus Henrique de Oliveria <mthws.henrique@hotmail.com>
2022-06-28 15:12:11 -0300

    Update aluno.dart

--- dart_application_1/lib/aula/aluno.dart
+++ dart_application_1/lib/aula/aluno.dart
@@ -2,14 +2,24 @@ import 'package:email_validator/email_validator.dart';
 
 main() {
   var aluno = Aluno(
-      RA: 20200024725, nome: "Matheus", sobrenome: "Henrique de Oliveira");
-  aluno._CPF = "***.741.38*-**";
+      RA: 20200024725,
+      nome: "Matheus",
+      sobrenome: "Henrique de Oliveira",
+      email: "mthws.henrique@hotmail.com",
+      cpf:"***.741.38*-**");
   aluno._anoNascimento = 2001;
-  aluno._email = "mthws.henrique@hotmail.com";
   print(aluno._anoNascimento);
   print(aluno.RA);
-  print(aluno._CPF);
-  print(aluno._email);
+   if (aluno.getValidateCPF() == true) {
+    print(aluno.cpf);
+  } else {
+    print("Digite um CPF válido");
+  }
+  if (aluno.getValidateEmail() == true) {
+    print(aluno.email);
+  } else {
+    print("Digite um e-mail válido");
+  }
   print(aluno.getNomeCompleto());
   print("${aluno.getIdadeAtual()}");
 }
@@ -18,33 +28,17 @@ class Aluno {
   int RA;
   String nome;
   String sobrenome;
-  late String _CPF;
-  late String _email;
+  String cpf;
+  String email;
   late int _anoNascimento;
 
   int get anoNascimento => _anoNascimento;
 
   set anoNascimento(int anoNascimento) => this._anoNascimento = anoNascimento;
 
-  String get CPF {
-    return this._CPF;
-  }
+  bool getValidateCPF() => this.cpf.length>=11 && this.cpf!=null; 
 
-  set CPF(String CPF) {
-    if (CPF.length == 11 && int.parse(CPF) != null) {
-      this._CPF = CPF;
-    }
-  }
-
-  String get email {
-    return this._email;
-  }
-
-  set email(String email) {
-    if ( email != null && !EmailValidator.validate(email)) {
-      this._email = email;
-    } 
-  }
+  bool getValidateEmail() => this.email != null && this.email!.contains("@");
 
   int getIdadeAtual() {
     var anoAtual = DateTime.now().year;
@@ -57,5 +51,10 @@ class Aluno {
 
   String? telefone;
 
-  Aluno({required this.RA, required this.nome, required this.sobrenome});
+  Aluno(
+      {required this.RA,
+      required this.nome,
+      required this.sobrenome,
+      required this.email,
+      required this.cpf});
 }
